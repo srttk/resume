5985431a4164092de266b821b47db5ad2c89647a
Sarath <saratonite@gmx.com>
2018-02-27 14:19:40 +0530

    :icecreame: Copy right year

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import './styles/index.scss';
 import packageJson from '../package.json'
 console.log(packageJson.description)
 console.log('Version : ', packageJson.version)
+
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYearSpan = document.querySelector('#current-year');
+    const now = new Date();
+    currentYearSpan.textContent = now.getFullYear();
+})
