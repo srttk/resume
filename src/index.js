65ec8bf55c8e955451e5fc64361679d8e110ed28
Sarath <saratonite@gmx.com>
2018-03-15 10:20:19 +0530

    :heart_eyes: work links

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
 import './styles/index.scss';
 import packageJson from '../package.json'
-console.log(packageJson.description)
+
+console.log('⚡️'+packageJson.description)
 console.log('Version : ', packageJson.version)
+console.log('%c 🔥 Oh my heavens! ⚡️ ', ' color: #5755d9; font-size:30px;');
 
 document.addEventListener('DOMContentLoaded', () => {
     const currentYearSpan = document.querySelector('#current-year');
