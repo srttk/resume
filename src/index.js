d13821d44cedde7bff119a453d6b9e366e14db2b
Sarath <sarath.hacks@gmail.com>
2019-05-10 16:39:09 +0530

    :tumbsup:

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
+import "./styles/index.scss";
+import packageJson from "../package.json";
 
-import './styles/index.scss';
-import packageJson from '../package.json'
+console.log("⚡️" + packageJson.description);
+console.log("Version : ", packageJson.version);
+console.log("%c 🔥 Oh my heavens! ⚡️ ", " color: #5755d9; font-size:30px;");
 
-console.log('⚡️'+packageJson.description)
-console.log('Version : ', packageJson.version)
-console.log('%c 🔥 Oh my heavens! ⚡️ ', ' color: #5755d9; font-size:30px;');
+document.addEventListener("DOMContentLoaded", () => {
+  const currentYearSpan = document.querySelector("#current-year");
+  const now = new Date();
+  currentYearSpan.textContent = now.getFullYear();
 
-document.addEventListener('DOMContentLoaded', () => {
-    const currentYearSpan = document.querySelector('#current-year');
-    const now = new Date();
-    currentYearSpan.textContent = now.getFullYear();
+  // Set back to home button
 
-    // Set back to home button
-
-    if(window.location && window.location.search.trim() === '?utm_source=sarath.tk') {
-
-        const gobackOrigin = document.querySelector('#go-back-origin')
-        gobackOrigin.href = 'https://sarath.tk';
-
-    }
-})
+  if (
+    window.location &&
+    window.location.search.trim() === "?utm_source=sarath.tk"
+  ) {
+    const gobackOrigin = document.querySelector("#go-back-origin");
+    gobackOrigin.href = "https://sarath.tk";
+  }
+});
